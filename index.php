c6a8da12b9997d8b95d642ad43937850053ff944
Daniel Mariselli <dmariselli@gmail.com>
2014-10-31 21:05:28 -0400

    added js script

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-<html>
-<head>
-	<title>MovieNight at the Disco</title>
-	<link rel="stylesheet" type="text/css" href="bootstrap-theme.css">
-	<link rel="stylesheet" type="text/css" href="main.css">
-</head>
-<body>
-	<p>Does this even work?</p>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+   		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>MovieNight at the Disco</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap-theme.css">
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+		<p>Does this even work?</p>
+
+		<script src="js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	</body>
 </html>
