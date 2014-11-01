2a2b5ae132aa71a3711714a2492a5fecc29ed67b
Daniel Mariselli <dmariselli@gmail.com>
2014-10-31 22:25:07 -0400

    changed colors

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
    		<meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>MovieNight at the Disco</title>
-
-		<link rel="stylesheet" href="css/bootstrap.min.css"
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
 		<link rel="stylesheet" type="text/css" href="css/main.css">
-		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
 		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
 		<link rel="icon" type="image/png" href="img/icon.jpg">
 	</head>
 	<body>
