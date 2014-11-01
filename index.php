b5805a59faa0d8ea9da4c731266e961516a85a74
Daniel Mariselli <dmariselli@gmail.com>
2014-10-31 21:20:57 -0400

    added nav bar

--- index.php
+++ index.php
@@ -5,11 +5,39 @@
    		<meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>MovieNight at the Disco</title>
-		<link rel="stylesheet" type="text/css" href="bootstrap-theme.css">
-		<link rel="stylesheet" type="text/css" href="main.css">
+
+		<link rel="stylesheet" href="css/bootstrap.min.css"
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/font-awesome.min.css">
 	</head>
 	<body>
-		<p>Does this even work?</p>
+
+
+	<div class="navbar navbar-default navbar-static-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav" id="main-menu">
+            <li><a href="#page-welcome">Home</a></li>
+            <li><a href="#page-about">About</a></li>
+            <li><a href="#page-resume">Movie</a></li>
+            <li><a href="#page-contact">Rating</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
 
 		<script src="js/bootstrap.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
