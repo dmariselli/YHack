644de4a64cf8b1fe1cb7908da6ff425d2575b64b
chounese <achou17@amherst.edu>
2014-10-31 21:02:42 -0400

    index.php test

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
 <html>
+<head>
+	<title>MovieNight at the Disco</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
 <body>
-
-<?php
-$str = "Hello world!";
-echo $str;
-?>
-
+	<p>Does this even work?</p>
 </body>
 </html>
