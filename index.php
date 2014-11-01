d177d689d374691545ca50ef098b3c2ca5e9ed25
Daniel Mariselli <dmariselli@gmail.com>
2014-11-01 16:53:58 -0400

    map implementation and reorganization

--- index.php
+++ index.php
@@ -52,10 +52,19 @@
 		        <header class="centering">
 		            <h1>Movie Night</h1>
 		            <p>Your source for 看电影!</p>
+
 		        </header>
 		   </div>
 		</div>
 	</section>
+
+<!-- MAP -->
+	<section id="map" class"map"></section>
+		<div cass="container"></div>
+			<h1>Movie Night</h1>
+            <p>Your source for 看电影!</p>
+			<div id="map-canvas"></div>
+
 
 <!-- Movie Thing -->
 
@@ -89,10 +98,99 @@
     <script type="text/javascript" src="js/jquery-1.11.1.js"></script>
     <script type="text/javascript" src="js/jquery.ui.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBuyBDatAQMaLRJ11BuSOc1OjFZHs6-U3U">
+    </script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?libraries=places"></script>
 
 	<script>
 
-		var bookinit = function(){
+	// maps.google.com
+	  var map;
+      var service;
+      var infowindow;
+
+      function initialize() {
+
+        if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(success);
+        } 
+        else {
+          error('Geo Location is not supported');
+        }
+      }
+
+      function success(position) {
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+      
+        var location = new google.maps.LatLng(lat,lng);
+
+        map = new google.maps.Map(document.getElementById('map-canvas'), {
+            center: location,
+            zoom: 15
+          });
+        var request = {
+          location: location,
+          radius: '500',
+          query: 'movie theatre'
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.textSearch(request, callback);
+      }
+
+      function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var j=0; j<results.length; j++){
+            createMarker(results[j]);
+          }
+        }
+      }
+
+      function createMarker(place){
+        infowindow=new google.maps.InfoWindow({
+          disableAutoPan: true,
+          map: map,
+          position: place.geometry.location,
+        })
+        var marker=new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+          var request={placeId: place.place_id};
+          service.getDetails(request, callback2);
+          marker.setTitle(place.name);
+          infowindow.close();
+          google.maps.event.addListener(marker, 'click', function() {
+            var printString="";
+            infowindow.open(map, this);
+            if (place.name!=null){
+              printString=printString+place.name+"<br />";
+            }
+            if (place.formatted_address!=null){
+              printString=printString+place.formatted_address+"<br />";
+            }
+            if (place.website!=null){
+              printString=printString+place.website+"<br />";
+            }
+            if (place.rating!=null){
+              printString=printString+place.rating+"<br />";
+            }
+            if (place.formatted_phone_number!=null){
+              printString=printString+place.formatted_phone_number;
+            }
+            infowindow.setContent(printString);
+          });
+      }
+
+      function callback2(place, status){
+        if (status == google.maps.places.PlacesServiceStatus.OK){
+          createMarker(place);
+        }
+      }
+      //movie stuff
+
+		var movieinit = function(){
 	        <?php
 				$host = 'localhost';
 				$user = 'root';
@@ -152,8 +250,9 @@
 	    {}).appendTo(wrapper);
 		};
 
-		bookinit();
+		movieinit();
 		generateMovieElement();
+		google.maps.event.addDomListener(window, 'load', initialize);
 
 	</script>
 
